0b74363e0192662ea9a888a848b6497199cd292b
AmanKamani <kamaniamanak@gmail.com>
2025-08-13 22:38:47 +0530

    Discovery-Service setup

---
+++ discoveryservice/src/test/java/com/fitness/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
