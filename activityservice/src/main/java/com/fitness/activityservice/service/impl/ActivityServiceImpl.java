89ccecc2c6566fa879288032f7d8f25eaf0e3a91
AmanKamani <kamaniamanak@gmail.com>
2025-08-15 00:55:44 +0530

    feat(activityservice): Added userid validation for trackActIvitiy

--- activityservice/src/main/java/com/fitness/activityservice/service/impl/ActivityServiceImpl.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/impl/ActivityServiceImpl.java
@@ -7,20 +7,26 @@ import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.repository.ActivityRepository;
 import com.fitness.activityservice.service.ActivityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ActivityServiceImpl implements ActivityService {
 
     private final ActivityRepository activityRepository;
+    private final WebClient userClient;
 
 
     @Override
     public ActivityResponse trackActivity(ActivityRequest request) {
-
+        validateUserId(request.getUserId());
         Activity savedActivity = activityRepository.save(ActivityMapper.from(request));
         return ActivityMapper.from(savedActivity);
     }
@@ -38,5 +44,22 @@ public class ActivityServiceImpl implements ActivityService {
         return activityRepository.findById(id)
                 .map(ActivityMapper::from)
                 .orElseThrow(() -> new RuntimeException("Not Found"));
+    }
+
+    public void validateUserId(String userId) {
+        try {
+            userClient.get()
+                    .uri("/api/v1/users/{userId}/validate", userId)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .block();
+        } catch (WebClientResponseException ex) {
+            if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                log.error("User ID not found: {}", userId);
+                throw new RuntimeException("User ID not found: " + userId);
+            }
+            log.error("Error validating user ID: {}", userId, ex);
+            throw new RuntimeException("Error while validating user ID", ex);
+        }
     }
 }
